c671ff6d8d5142b9d5610f41f877ded07a65d157
Paresh Pandit <paresh.pandit3193@gmail.com>
2021-08-14 14:17:28 -0700

    LongestSubstringWithoutRepeatingCharacters

---
+++ Java/1TwoSum/LongestSubStringWithoutRepeatingCharacters.java
@@ -0,0 +1,55 @@
+/*
+Given a string s, find the length of the longest substring without repeating characters.
+
+ 
+
+Example 1:
+
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+Example 2:
+
+Input: s = "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+Example 3:
+
+Input: s = "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3.
+Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+Example 4:
+
+Input: s = ""
+Output: 0
+
+
+Runtime: 6 ms, faster than 61.25% of Java online submissions for Longest Substring Without Repeating Characters.
+Memory Usage: 39.4 MB, less than 37.39% of Java online submissions for Longest Substring Without Repeating Characters.
+*/
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int i =0; 
+        int  j = 0;
+        int max = 0;//Integer.MIN_VALUE;
+        HashSet<Character> hm = new HashSet<Character>();
+        while(i < s.length()) 
+        {
+            if(!hm.contains(s.charAt(i)))
+            {
+                hm.add(s.charAt(i));
+                if(max < hm.size())
+                {
+                    max = hm.size();
+                }
+                i++;
+            }
+            else{
+                hm.remove(s.charAt(j++));
+            }
+        }
+        return max;
+        
+}
+}
